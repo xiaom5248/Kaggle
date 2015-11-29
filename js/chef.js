8c1b9de28152a1b813db6e44f86f6b59c6a411af
Xx-william <e-wangxi@hotmail.com>
2015-11-29 17:05:55 +0100

    calenderier

--- js/chef.js
+++ js/chef.js
@@ -24,7 +24,17 @@ $("#regarder").on('click',function(){
 });
 
 
-
+    $('.form_datetime').datetimepicker({
+        language:  'fr',
+        weekStart: 1,
+        todayBtn:  1,
+		autoclose: 1,
+		todayHighlight: 1,
+		startView: 2,
+		forceParse: 0,
+        showMeridian: 1
+    });
+	
 
 var commandcode ,username,teamid,index_id;
 $(document).ready(function() {
